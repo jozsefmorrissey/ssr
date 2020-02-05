e9684f852abb863098f7d753c8ccbbf029cc71d3
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2020-02-05 14:05:42 -0600

    compile error fixed

--- src/common/CommandSettings.cpp
+++ src/common/CommandSettings.cpp
@@ -19,6 +19,8 @@ along with SimpleScreenRecorder.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "CommandSettings.h"
 
+CommandSettings::init = false;
+
 CommandSettings::Initialize(QString _output_file,
 			QString _audio_profile_name,
 			QString _video_profile_name,
