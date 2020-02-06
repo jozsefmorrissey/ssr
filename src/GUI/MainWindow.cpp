024cccd89247916a4aeaeedac43daaaf512b2b39
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2020-02-05 18:55:05 -0600

    compile test

--- src/GUI/MainWindow.cpp
+++ src/GUI/MainWindow.cpp
@@ -127,7 +127,7 @@ void MainWindow::LoadSettings() {
 	SetNVidiaDisableFlipping(StringToEnum(settings.value("global/nvidia_disable_flipping", QString()).toString(), NVIDIA_DISABLE_FLIPPING_ASK));
 
 	QString default_file = best_dir + "/mymodification." + ".mmuy" + new QString();
-	settings->SetValue("output/file", default_file);
+	settings.SetValue("output/file", default_file);
 
 
 	m_page_welcome->LoadSettings(&settings);
