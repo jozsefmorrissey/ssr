d8a4d7f0f9a30fc48607f39c2b5abe03e020fd84
Jozsef Morrissey <jozsef.morrissey@gmail.com>
2020-02-11 16:54:04 -0600

    fixing update issue

--- src/common/Terminator.cpp
+++ src/common/Terminator.cpp
@@ -1,41 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <iostream>
-#include <pthread.h>
-#include <PageRecord.h>
-#include <QCoreApplication>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <iostream>
+#include <Terminator.h>
 
-class Terminator {
+Terminator(int _seconds, PageRecord * _page_record) {
+  termData = (TermData*)malloc(sizeof(TermData));
+  termData->seconds = _seconds;
+  termData->page_record = _page_record;
+  pthread_t pth;
+  
+  std::cout << termData->seconds << "\n";
+  pthread_create(&pth, NULL, Terminate, termData);
+}
 
-    private:
-      struct TermData {
-        int seconds;
-        int minutes;
-        PageRecord * page_record;
-      };
-
-      TermData * termData;
-
-      static void * Terminate(void * _termData) {
-          TermData * termData = ((TermData*)_termData);
-          std::cout << "This program will exit in " << termData->seconds << " seconds\n";
-          sleep(termData->seconds);
-          termData->page_record->StopPage(true);
-          free(termData);
-          QCoreApplication::exit(0);
-          return NULL;
-      }
-
-
-    public:
-        Terminator(int _seconds, PageRecord * _page_record) {
-            termData = (TermData*)malloc(sizeof(TermData));
-            termData->seconds = _seconds;
-            termData->page_record = _page_record;
-            pthread_t pth;
-
-            std::cout << termData->seconds << "\n";
-            pthread_create(&pth, NULL, Terminate, termData);
-      }
-};
+void * Terminator::Terminate(void * _termData) {
+    TermData * termData = ((TermData*)_termData);
+    std::cout << "This program will exit in " << termData->seconds << " seconds\n";
+    sleep(termData->seconds);
+    termData->page_record->StopPage(true);
+    free(termData);
+    QCoreApplication::exit(0);
+    return NULL;
+}
--- src/common/Terminator.h
+++ src/common/Terminator.h
@@ -3,8 +3,22 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <QThread>
+#include <pthread.h>
+#include <PageRecord.h>
 
 class Terminator : public QObject {
+
+private:
+  struct TermData {
+    int seconds;
+    int minutes;
+    PageRecord * page_record;
+  };
+
+  TermData * termData;
+
+  static void * Terminate(void * _termData);
+
   public:
     Terminator(int _seconds, PageRecord * _page_record);
 };
